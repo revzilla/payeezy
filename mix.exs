46578d9312912f2f0caaa1ca441979a4686142ea
Tyler Cain <tyler.cain@revzilla.com>
2017-02-24 14:34:33 -0500

    remove decimal from apps

--- mix.exs
+++ mix.exs
@@ -17,7 +17,7 @@ defmodule Payeezy.Mixfile do
   end
 
   def application do
-    [applications: [:logger, :poison, :decimal, :httpoison]]
+    [applications: [:logger, :poison, :httpoison]]
   end
 
   defp description do
